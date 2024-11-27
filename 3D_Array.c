270af05ddb8324230c93ac64fdcee392d29d3437
Haseebahmed8t <haseeb8t@gmail.com>
2024-11-27 15:24:36 +0500

    Add files via upload

---
+++ 3D_Array.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(){
+	
+	int array[3][2][2] = {{{1,2},{3,4}}, {{5,6}, {7,8}}, {{9,10}, {11,12}}};
+	int i,j,k;
+	for(i=0; i<3; i++){
+		for(j=0; j<2; j++){
+			for(k=0; k<2; k++){
+				printf("Array[%d][%d][%d] = %d \n", i,j,k,array[i][j][k]);
+			}
+		}
+	}
+}
+
